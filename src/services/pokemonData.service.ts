d67bb8aa5e01339c90c4d91fedba4f2e66e5e400
Yiomar Ruiz <yiomar.rc@gmail.com>
2022-10-11 08:58:50 -0500

    Add readjustment to service

--- src/services/pokemonData.service.ts
+++ src/services/pokemonData.service.ts
@@ -3,7 +3,6 @@ import Axios from 'axios';
 
 export const getPokemonData = (id: string) => {
   const controller = axiosAbort();
-  console.log('process.env.VITE_APP_API', process.env.VITE_APP_API);
   return {
     call: Axios.get(`${process.env.VITE_APP_API}/pokemon/${id}`, {
       signal: controller.signal,
