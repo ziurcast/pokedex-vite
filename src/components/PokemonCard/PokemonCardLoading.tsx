bbf400e60c3456bf55a6a3ee547765c55a319c69
Yiomar Ruiz <yiomar.rc@gmail.com>
2022-10-14 17:20:46 -0500

    Add readjustment to load card width

--- src/components/PokemonCard/PokemonCardLoading.tsx
+++ src/components/PokemonCard/PokemonCardLoading.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const PokemonCardLoading = () => {
   return (
-    <div className="animate-pulse w-1/2 md:w-1/3 lg:w-1/5 p-3">
+    <div className="animate-pulse w-64">
       <div
         className={
           'p-2 h-72 bg-gradient-to-b from-gray-light to-white flex items-center justify-center rounded-lg drop-shadow-lg'
